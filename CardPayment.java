cd72f78f514a09b53a5166727932ecb80d51bad3
Aden Yu <adenyu@iu.edu>
2026-05-25 00:17:19 -0700

    Delete CardPayment.java

--- CardPayment.java
+++
@@ -1,7 +0,0 @@
-public class CardPayment implements PaymentStrategy {
-    @Override
-    public boolean pay(double amount) {
-        System.out.println("Paid $" + String.format("%.2f", amount) + " using card.");
-        return true;
-    }
-}
